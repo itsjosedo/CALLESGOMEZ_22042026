c255438ea97520997278b78eb754f31bea21da8e
Jose Douglas Calles Gómez <josedocallesgmz@gmail.com>
2026-04-27 17:52:26 -0600

    Finalizacion de practica

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const ctx = canvas.getContext("2d");
 
 //DATOS PARA GRAFICAR
 const labels = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sept', 'Oct', 'Nov', 'Dic'];
-const sanSalvador = [30, 32, 34, 35, 36, 37, 36, 35, 34, 32, 31, 30];
-const santaTecla = [25,26,28, 29, 31, 32, 32, 31, 30, 28, 27, 26];
+const sanSalvador = [30, 32, 34, 35, 36, 37, 36, 35, 34, 32, 31, 30]; //TEMEPARTURAS REGISTARDAS EN SAN SALVADOR POR MES
+const santaTecla = [25,26,28, 29, 31, 32, 32, 31, 30, 28, 27, 26]; //TEMPERATURAS REGISTRADAS EN SANTA TECLA POR MES
 
-const marginLeft = 50;
-const marginRight = 50;
+const marginLeft = 50; //MARGEN A LA IZQUIERDA
+const marginRight = 50; //MARGEN A LA DERECHA
 
 function drawLineWithLabels (data, color) {
     ctx.beginPath();
@@ -15,14 +15,15 @@ function drawLineWithLabels (data, color) {
     ctx.strokeStyle = color;
 
     //SE RECORRE CADA VALOR DE data
-    for(let i = 0; i<data.lenght; i++){
+    for(let i = 0; i<data.length; i++){
 
         //SE AJUSTA POSICIÓN DE LA GRÁFICA
-        const x = (i/(data.lenght - 1)) * (canvas.width - marginLeft - marginRight) + marginLeft;
-        const y =  canvas.heigth() (data[i]-15) * 10;
+        const x = (i/(data.length - 1)) * (canvas.width - marginLeft - marginRight) + marginLeft;
+        const y =  canvas.height - (data[i]-15) * 10;
 
         //COMENZAMOS A DIBUJAR
         if(i === 0) {
+            //CUANDO INICIA LA LISTA
             ctx.moveTo(x,y);
         }else{
             ctx.lineTo(x,y);
@@ -30,15 +31,51 @@ function drawLineWithLabels (data, color) {
 
         ctx.fillStyle = color;
         ctx.font = "12px Arial";
-        ctx.fillText(data[i]+"°C", x + 5 ,y-5); //SE ESCRIBE EL VALOR DE LA ETIQUETA
+        ctx.fillText(data[i]+"°C", x + 5 ,y - 5); //SE ESCRIBE EL VALOR DE LA ETIQUETA
     }
     ctx.stroke();
 }
 
-/*
-function drawAxes() { I
+
+function drawAxes() {
     ctx.beginPath();
     ctx.strokeStyle = "#000000";
     ctx.lineWidth = 1;
-    ctx.moveTo();
-}*/
+
+    //DIBUJA EJE X  
+    ctx.moveTo(50, canvas.height - 50);
+    ctx.lineTo(canvas.width - 50, canvas.height - 50);
+
+    //DIBUJA EJE Y
+    ctx.moveTo(50, canvas.height - 50);
+    ctx.lineTo(50, 50);
+
+    ctx.stroke();
+
+    //eSCRIBIR ETIQUETAS en eje x
+    for(let i=0; i < labels.length; i++){
+        const x = (i/(labels.length - 1)) * (canvas.width - 100) + 50; //
+        ctx.fillText(labels[i],x,canvas.height - 30);
+    }
+
+    //escribir etiquetas en eje y
+    for(let i = 20; i<=40; i+=5){
+        const y = canvas.height - 50 - (i - 20) * 10;
+        ctx.fillText(i+" °C", 20, y + 5);
+
+    }
+}
+
+drawAxes();
+drawLineWithLabels(sanSalvador, 'red');
+drawLineWithLabels(santaTecla, 'blue');
+
+ctx.fillStyle = "red";
+ctx.fillRect(70,20,10,10);
+ctx.fillStyle = "black";
+ctx.fillText("San Salvador", 85,30);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(170,20,10,10)
+ctx.fillStyle = "black";
+ctx.fillText("Santa Tecla", 185,30);
